eae73df61432bc81d7ab35ee4c541d2abf429b31
Md Arif Hossain <arif.swfu@outlook.com>
2022-02-17 11:57:04 +0800

    Update App.js

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 
 const App = () => {
-  const [state, setState] = useState({});
+  const [state, setState] = useState([]);
 
   useEffect(() => {
-    fetch(`https://my-json-server.typicode.com/arifpro/json-api/posts`)
+    fetch(`https://my-json-server.typicode.com/arifpro/json-api`)
       .then((res) => res.json())
-      .then((data) => setState((_state) => ({ ..._state, posts: data })))
+      .then((data) => setState((_state) => ({ ..._state, posts: data.posts })))
       .catch((err) => console.log(err));
   }, []);
 
@@ -15,6 +15,7 @@ const App = () => {
   return (
     <section>
       <h1>JSON-API</h1>
+      {state?.posts?.map((post) => <div key={post.id}>{post.title}</div>)}
     </section>
   );
 };
